ea33ad572e6b80def6d5e728926f1f7ec178232c
Lhourquin <lhourquin@gmail.com>
2019-05-15 17:01:06 +0200

    last exemple conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import Clock from './Clock';
 import ActionClick from './ActionClick';
 import Toggle from './Toggle';
 import LoginControl from './LoginControl';
-
+import Page from './Page'
 
 class App extends Component {
 
     render() {
+
+        function Mailbox(props){
+            const unreadMessages = props.unreadMessages;
+            return(
+                <div>
+                    <h1>Bonjour !</h1>
+                    {unreadMessages.length > 0 && 
+                    <h2>
+                        Vouavez {unreadMessages.length} message(s) non lu(s).
+                    </h2>
+                }
+                </div>
+            )
+        }
+
+        const messages = ['sdf', 'gfd','hui,','trtro'];
+
         return (
             <div>
                 <Clock/>
                 <ActionClick/>
                 <Toggle/>
+                <Page/>
                 <LoginControl/>
-                <Clock/>
-                <Clock/>
+                <Mailbox unreadMessages={messages}/>
             </div>
         )
     }
